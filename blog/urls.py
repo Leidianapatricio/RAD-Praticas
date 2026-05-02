58ed8deaaa6b7f1efd92bf3d8c94a1b641455ea0
Leidianapatricio <leidiana.patricio@academico.ifpb.edu.br>
2026-05-02 14:32:56 -0300

    Pratica 03 - Templates Django

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("welcome", views.welcome, name="welcome"),
-    path("eco/<str:texto>/", views.eco, name="eco"),
-    path("info/", views.info, name="info"),
-    ]
+    path("", views.home, name="home"),
+    path("contato/<str:telefone>/", views.contato, name="contato"),
+]
+
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
+from datetime import datetime
 
-from django.http import HttpResponse, JsonResponse
 
-def welcome(request):
-    return HttpResponse("Bem-vindo ao meu blog!")
-
-def eco(request, texto):
-    return HttpResponse(f"Você digitou: {texto}")
-
-def info(request):
-    dados = {
-        "disciplina": "RAD",
-        "framework": "Django",
-        "semestre" : "2026.1"
+def home(request):
+    contexto = {
+        "nome": "Leidiana",
+        "data": datetime.now(),
+        "is_logged_in": True,
+        "role": "admin",
+        "produtos": [
+            {"nome": "Notebook", "preco": 3000},
+            {"nome": "Mouse", "preco": 100},
+            {"nome": "Teclado", "preco": 200},
+        ]
     }
-    return JsonResponse(dados)
+    return render(request, "blog/home.html", contexto)
+
+def contato(request, telefone):
+    return render(request, "blog/contato.html", {"telefone": telefone})
+
+
+
 
 
