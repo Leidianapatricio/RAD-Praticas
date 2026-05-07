02606137ffe932cffb6fe529f21a192b0758ba5d
Leidianapatricio <leidiana.patricio@academico.ifpb.edu.br>
2026-05-07 18:37:15 -0300

    Pratica 04 - Models e ORM Django

---
+++ edu/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Autor, Editora, Livro, Publica
+
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Publica)
+
+
